531835c43f5cbec3c2d76e6e7d4d995f1c37b9c8
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2022-12-25 18:42:31 +0200

    Add files via upload

---
+++ assets/preload/data/unemployed/script.lua
@@ -0,0 +1,12 @@
+function onStepHit()
+if curBeat == 312 then
+	 setProperty('defaultCamZoom',1.3)
+ followchars = false;
+
+ end
+
+ if curBeat == 320 then
+	 setProperty('defaultCamZoom',0.9)
+ followchars = false;
+   end
+end
